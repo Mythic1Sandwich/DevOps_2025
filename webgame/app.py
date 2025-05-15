378c2038e75154deca62288e0f6e5850139839de
rail <shakirovrl97@gmail.com>
2025-05-15 18:13:33 +0000

    tests

--- webgame/app.py
+++ webgame/app.py
@@ -8,7 +8,7 @@ application = app
 
 app.config['SECRET_KEY'] = '6342eb430febed6def4d4063a2fa717907fb4911143de1891c59dca9e0519bd8'
 
-app.config['DATABASE'] = r'var/jenkins_home/workspace/tests/webgame/gamemerch.db'
+app.config['DATABASE'] = r'gamemerch.db'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
